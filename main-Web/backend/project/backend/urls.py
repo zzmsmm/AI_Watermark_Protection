a3131bd68ace6a9c36c991e60987b3483e6aea03
zzmsmm <zzmsmm@sjtu.edu.cn>
2022-05-04 00:52:12 +0800

    合并zwj

--- main-Web/backend/project/backend/urls.py
+++ main-Web/backend/project/backend/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     re_path('register/', views.register),
     re_path('getinfo/', views.getinfo),
     re_path('changeavatar/', views.change_avatar),
+    re_path('certification_apply/', views.certification_apply),
+    re_path('certification_list/', views.certification_list),
+    re_path('unfinished_list/', views.unfinished_list),
+    re_path('unfinished_detail/', views.unfinished_detail),
+    re_path('finished_apply/', views.finished_apply)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
