9e22193c06abad820a55e10d4e30988cc190a43a
zzmsmm <zzmsmm@sjtu.edu.cn>
2022-05-04 00:53:40 +0800

    合并zwj，并对unfinished_list进行数据交互测试（成功），修改详见代码

--- main-Web/backend/project/backend/views.py
+++ main-Web/backend/project/backend/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect
 import os
 import json
 import hashlib
+import random
+import time
 from . import models
 
 
@@ -122,3 +124,139 @@ def getinfo(request):
         }
     }
     return JsonResponse(resp)
+
+
+# 先产生随机数，再取其md5为密钥
+def generate_key():
+    rand_num = random.randint(0, 2147483647)
+    return hashlib.md5(str(rand_num).encode('utf-8')).hexdigest()
+
+
+@require_http_methods(["POST"])
+def certification_apply(request):
+    body_json = request.body.decode()
+    body_dict = json.loads(body_json)
+
+    token = body_dict['token']
+    user = models.User.objects.get(token=token)
+    new_request = models.RequestInfo()
+    new_request.user_name = user.user_name
+    new_request.watermark_type = body_dict['watermark_type']
+    new_request.model_type = body_dict['model_type']
+    new_request.key = generate_key()
+    new_request.hash = hashlib.md5(new_request.key.encode('utf-8')).hexdigest()
+    new_request.save()
+    return JsonResponse(new_request)
+
+
+@require_http_methods(["GET"])
+def certification_list(request):
+    token = request.GET.get('token')
+
+    # 先通过token获取用户名
+    try:
+        user = models.User.objects.get(token=token)
+    except models.User.DoesNotExist:
+        return JsonResponse({
+            "code": 60204,
+            "message": "User Not Found"
+        })
+
+    # 再通过用户名获取记录
+    try:
+        record = models.AuthenticationRecord.objects.get(user_name=user.user_name)
+    except models.AuthenticationRecord.DoesNotExist:
+        return JsonResponse({
+            "code": 60204,
+            "message": "Record Not Found"
+        })
+    resp = {
+        "code": 20000,
+        "data": dict(record)
+    }
+    return JsonResponse(resp)
+
+
+# 测试成功（多条数据返回如何处理，如何在json中添加新元素，推荐算法查询）
+@require_http_methods(["GET"])
+def unfinished_list(request):
+    token = request.GET.get('token')
+    print(token)
+    # 先通过token获取用户名
+    try:
+        user = models.User.objects.get(token=token)
+    except models.User.DoesNotExist:
+        return JsonResponse({
+            "code": 60204,
+            "message": "User Not Found"
+        })
+    print(user)
+    # 再通过用户名获取记录
+    try:
+        data = models.RequestInfo.objects.filter(user_name=user.user_name)
+    except models.RequestInfo.DoesNotExist:
+        return JsonResponse({
+            "code": 60204,
+            "message": "Record Not Found"
+        })
+    data = list(data.values())
+    print(data)
+    for row in data:
+        algorithm = models.RecommendAlgorithm.objects.filter(watermark_type=row['watermark_type'],
+                                                             model_type=row['model_type'])
+        row['algorithm_name'] = algorithm[0].algorithm_name
+    print(data)
+    resp = {
+        "code": 20000,
+        "data": data
+    }
+    return JsonResponse(resp)
+
+
+@require_http_methods(["POST"])
+def unfinished_detail(request):
+    file = request.FILES.get('detail_file')
+    hash = request.POST.get('hash')
+    sql_path = f"{os.getcwd()}/detail/{hash}"
+    with open(sql_path, 'wb') as f:
+        for content in file.chunks():
+            f.write(content)
+    resp = {
+        "code": 20000,
+        "message": 'success',
+    }
+    return JsonResponse(resp)
+
+
+# 这个地方其实应该就是把RequestInfo里的数据转到AuthenticationData和AuthenticationRecord里
+@require_http_methods(["POST"])
+def finished_apply(request):
+    body_json = request.body.decode()
+    body_dict = json.loads(body_json)
+    token = body_dict.get('token')
+    hash = body_dict.get('hash')
+    record = models.RequestInfo.objects.get(hash)
+
+    new_finished_record = models.AuthenticationRecord()
+    new_finished_data = models.AuthenticationData()
+
+    new_finished_data.hash = hash
+    new_finished_data.authentication_data_path = f"{os.getcwd()}/detail/{hash}"
+    new_finished_data.save()
+
+    new_finished_record.key = record.key
+    new_finished_record.hash = record.hash
+    new_finished_record.user_name = record.user_name
+    new_finished_record.watermark_type = record.watermark_type
+    new_finished_record.model_type = record.model_type
+    new_finished_record.timestamp = time.time()
+    new_finished_record.save()
+
+    record.delete()
+
+    resp = {
+        "code": 20000,
+        "message": "success"
+    }
+    return JsonResponse(resp)
+
